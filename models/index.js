d377ce7a57e0c1326a9de36d35463dd68873a74b
jeffDevelops <jefferydreynolds@gmail.com>
2017-08-28 13:07:45 -0600

    hello?

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6*/
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/same-name-social-experiment');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/same-name-social-experiment');
 
 module.exports.Traffic = require('./traffic');
